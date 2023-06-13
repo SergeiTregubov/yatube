17c9862eaad6a4f9e9c685cbeffc35e2dc522a88
Your Name <you@example.com>
2023-06-13 16:11:01 +0300

    Readme

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -130,3 +130,4 @@ def profile_unfollow(request, username):
     author = get_object_or_404(User, username=username)
     Follow.objects.filter(user=request.user, author=author).delete()
     return redirect('posts:profile', username=author)
+
